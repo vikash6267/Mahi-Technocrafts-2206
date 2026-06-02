ad82378ec5ce17a5dbde1257463cc6efc9e42741
Vikash <vikasmaheshwari6267@gmail.com>
2026-06-02 20:02:08 +0600

    explicitly allow AI web crawlers like GPTBot and PerplexityBot in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,14 +4,28 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://mahitechnocrafts.in';
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/admin/',
-        '/api/admin/'
-      ]
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/admin/'
+        ]
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/'
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/'
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/'
+      }
+    ],
     sitemap: `${baseUrl}/sitemap.xml`
   };
 }
